aaf03f046e808f3504932b636cf3e520d4a34191
rajshakya123 <rajendrasingh4719@gmail.com>
2024-07-18 11:52:40 +0530

    thursday commit
    ref #master

--- src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { counterReducer } from "./counterReducers";
-
-const rootReducer = combineReducers({
-    webrtcReducer: counterReducer,
-
-});
-export default rootReducer;
